73f24e45b572d385f142517221c57979c1939111
Garongan <alvindo56@gmail.com>
2023-05-26 09:49:49 +0700

    update mencoba svc

--- index.php
+++ index.php
@@ -5,7 +5,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 // pilih algoritma untuk melakukan klasifikasi
 
 // Decision Tree
-include('./DC.php');
+// include('./DC.php');
 
 // Naive Bayes
 // include('./NB.php');
+
+// SVC
+include('./SVC.php');
